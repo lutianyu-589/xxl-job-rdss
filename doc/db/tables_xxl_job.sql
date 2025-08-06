1e096afbb732fd247c7245838e97caf6b1f3bf87
帆帆没法变猫猫 <1950698@tongji.edu.cn>
2025-08-06 16:50:24 +0800

    新增cq表

--- doc/db/tables_xxl_job.sql
+++ doc/db/tables_xxl_job.sql
@@ -2,20 +2,22 @@
 # XXL-JOB
 # Copyright (c) 2015-present, xuxueli.
 
-CREATE database if NOT EXISTS `xxl_job` default character set utf8mb4 collate utf8mb4_unicode_ci;
+CREATE database if NOT EXISTS `xxl_job` default character set utf8mb4 collate utf8mb4_bin;
 use `xxl_job`;
 
 SET NAMES utf8mb4;
 
 CREATE TABLE `xxl_job_info`
 (
-    `id`                        int(11)      NOT NULL AUTO_INCREMENT,
+    `id`                        bigint(20)      NOT NULL AUTO_INCREMENT,
     `job_group`                 int(11)      NOT NULL COMMENT '执行器主键ID',
+    `job_name`                  varchar(255) NOT NULL COMMENT '作业名',
     `job_desc`                  varchar(255) NOT NULL,
     `add_time`                  datetime              DEFAULT NULL,
     `update_time`               datetime              DEFAULT NULL,
     `author`                    varchar(64)           DEFAULT NULL COMMENT '作者',
     `alarm_email`               varchar(255)          DEFAULT NULL COMMENT '报警邮件',
+    `alarm_contacts`            varchar(255)          DEFAULT NULL COMMENT '报警联系人',
     `schedule_type`             varchar(50)  NOT NULL DEFAULT 'NONE' COMMENT '调度类型',
     `schedule_conf`             varchar(128)          DEFAULT NULL COMMENT '调度配置，值含义取决于调度类型',
     `misfire_strategy`          varchar(50)  NOT NULL DEFAULT 'DO_NOTHING' COMMENT '调度过期策略',
@@ -33,15 +35,16 @@ CREATE TABLE `xxl_job_info`
     `trigger_status`            tinyint(4)   NOT NULL DEFAULT '0' COMMENT '调度状态：0-停止，1-运行',
     `trigger_last_time`         bigint(13)   NOT NULL DEFAULT '0' COMMENT '上次调度时间',
     `trigger_next_time`         bigint(13)   NOT NULL DEFAULT '0' COMMENT '下次调度时间',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`job_name`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE `xxl_job_log`
 (
-    `id`                        bigint(20) NOT NULL AUTO_INCREMENT,
-    `job_group`                 int(11)    NOT NULL COMMENT '执行器主键ID',
-    `job_id`                    int(11)    NOT NULL COMMENT '任务，主键ID',
+    `id`                        bigint(20)  NOT NULL AUTO_INCREMENT,
+    `job_group`                 bigint(20)   NOT NULL COMMENT '执行器主键ID',
+    `job_id`                    bigint(20)    NOT NULL COMMENT '任务，主键ID',
     `executor_address`          varchar(255)        DEFAULT NULL COMMENT '执行器地址，本次执行的地址',
     `executor_handler`          varchar(255)        DEFAULT NULL COMMENT '执行器任务handler',
     `executor_param`            varchar(512)        DEFAULT NULL COMMENT '执行器任务参数',
@@ -64,7 +67,7 @@ CREATE TABLE `xxl_job_log`
 
 CREATE TABLE `xxl_job_log_report`
 (
-    `id`            int(11) NOT NULL AUTO_INCREMENT,
+    `id`            bigint(20) NOT NULL AUTO_INCREMENT,
     `trigger_day`   datetime         DEFAULT NULL COMMENT '调度-时间',
     `running_count` int(11) NOT NULL DEFAULT '0' COMMENT '运行中-日志数量',
     `suc_count`     int(11) NOT NULL DEFAULT '0' COMMENT '执行成功-日志数量',
@@ -77,7 +80,7 @@ CREATE TABLE `xxl_job_log_report`
 
 CREATE TABLE `xxl_job_logglue`
 (
-    `id`          int(11)      NOT NULL AUTO_INCREMENT,
+    `id`          bigint(20)      NOT NULL AUTO_INCREMENT,
     `job_id`      int(11)      NOT NULL COMMENT '任务，主键ID',
     `glue_type`   varchar(50) DEFAULT NULL COMMENT 'GLUE类型',
     `glue_source` mediumtext COMMENT 'GLUE源代码',
@@ -90,7 +93,7 @@ CREATE TABLE `xxl_job_logglue`
 
 CREATE TABLE `xxl_job_registry`
 (
-    `id`             int(11)      NOT NULL AUTO_INCREMENT,
+    `id`             bigint(20)      NOT NULL AUTO_INCREMENT,
     `registry_group` varchar(50)  NOT NULL,
     `registry_key`   varchar(255) NOT NULL,
     `registry_value` varchar(255) NOT NULL,
@@ -102,7 +105,7 @@ CREATE TABLE `xxl_job_registry`
 
 CREATE TABLE `xxl_job_group`
 (
-    `id`           int(11)     NOT NULL AUTO_INCREMENT,
+    `id`           bigint(20)     NOT NULL AUTO_INCREMENT,
     `app_name`     varchar(64) NOT NULL COMMENT '执行器AppName',
     `title`        varchar(12) NOT NULL COMMENT '执行器名称',
     `address_type` tinyint(4)  NOT NULL DEFAULT '0' COMMENT '执行器地址类型：0=自动注册、1=手动录入',
@@ -114,7 +117,7 @@ CREATE TABLE `xxl_job_group`
 
 CREATE TABLE `xxl_job_user`
 (
-    `id`         int(11)     NOT NULL AUTO_INCREMENT,
+    `id`         bigint(20)     NOT NULL AUTO_INCREMENT,
     `username`   varchar(50) NOT NULL COMMENT '账号',
     `password`   varchar(50) NOT NULL COMMENT '密码',
     `role`       tinyint(4)  NOT NULL COMMENT '角色：0-普通用户、1-管理员',
@@ -131,6 +134,25 @@ CREATE TABLE `xxl_job_lock`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
+CREATE TABLE xxl_job_cq
+(
+    cq_no varchar(255) comment 'cq单号',
+    cq_name varchar(255) comment 'cq单名',
+    prod_date date comment '投产日期',
+    create_time datetime(6) default current_timestamp(6) comment '创建日期',
+    update_time datetime(6) default current_timestamp(6) on update current_timestamp(6) comment '更新日期'
+)ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE xxl_job_cq_job
+(
+        cq_no varchar(255) comment 'cq单号',
+        job_name varchar(255) comment '作业名',
+        rel_type varchar(255) comment '类型',
+        create_time datetime(6) default current_timestamp(6) comment '创建日期',
+        update_time datetime(6) default current_timestamp(6) on update current_timestamp(6) comment '更新日期'
+)ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
 
 ## —————————————————————— init data ——————————————————
 
